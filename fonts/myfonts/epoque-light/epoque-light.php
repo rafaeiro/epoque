cc0ac12f096018576e80c6231ce8af86e9d91e03
rafaeiro <92637778+rafaeiro@users.noreply.github.com>
2021-12-14 13:19:10 -0300

    limpeza de dados

---
+++ fonts/myfonts/epoque-light/epoque-light.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Plugin Name: epoque-light #153801
+Plugin URI: https://www.myfonts.com
+Description: MyFonts WordPress Plugin
+Version: 0.1
+Author: MyFonts Team
+Author URI: https://www.myfonts.com
+*/
+
+if ( version_compare( PHP_VERSION, '5.4.0', '<') ) {
+    die("Cannot activate MyFonts plugin because your PHP version is out of date. <br />Please use at least PHP 5.4 (currently using ".PHP_VERSION.") with the MyFonts WordPress plugin.");
+}
+
+require_once('installer.inc');
+if(is_admin()) {require_once( 'mf-options.inc' ); }
+add_action( 'admin_init', array('MyFontsInstaller_153801','checkInstallation'));
+add_action( 'wp_enqueue_scripts', array('MyFontsInstaller_153801','myfonts_loadupWebfonts' ));
