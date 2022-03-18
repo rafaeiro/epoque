53f034034f7265f7db5d5616b1c87197a0417432
rafaeiro <92637778+rafaeiro@users.noreply.github.com>
2022-03-18 11:18:56 -0300

    mudanças desconhecidas

--- fonts/myfonts/epoque/epoque.php
+++ fonts/myfonts/epoque/epoque.php
@@ -10,10 +10,10 @@ Author URI: https://www.myfonts.com
 */
 
 if ( version_compare( PHP_VERSION, '5.4.0', '<') ) {
-    die("Cannot activate MyFonts plugin because your PHP version is out of date. <br />Please use at least PHP 5.4 (currently using ".PHP_VERSION.") with the MyFonts WordPress plugin.");
+  die("Cannot activate MyFonts plugin because your PHP version is out of date. <br />Please use at least PHP 5.4 (currently using ".PHP_VERSION.") with the MyFonts WordPress plugin.")
 }
 
-require_once('installer.inc');
-if(is_admin()) {require_once( 'mf-options.inc' ); }
-add_action( 'admin_init', array('MyFontsInstaller_314623','checkInstallation'));
-add_action( 'wp_enqueue_scripts', array('MyFontsInstaller_314623','myfonts_loadupWebfonts' ));
+require_once('installer.inc')
+if(is_admin()) {require_once( 'mf-options.inc' ) }
+add_action( 'admin_init', array('MyFontsInstaller_314623','checkInstallation'))
+add_action( 'wp_enqueue_scripts', array('MyFontsInstaller_314623','myfonts_loadupWebfonts' ))
